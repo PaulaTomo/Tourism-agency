c61d6816886ca838e835af6f35dac06e37c282e9
PaulaTomo <paulatomoiaga91@yahoo.com>
2023-03-13 19:22:11 +0100

    edit

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -9,12 +9,12 @@ function Contact(){
             <div className="container-fluid">
                 <div className="card-contact px-sm-4 col-6">
                     <div className='col-lg-5'>
-                        <iframe title="map">
+                        <iframe title="map"
                             src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12094.57348593182!2d-74.00599512526003!3d40.72586666928451!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2598f988156a9%3A0xd54629bdf9d61d68!2sBroadway-Lafayette%20St!5e0!3m2!1spl!2spl!4v1624523797308!5m2!1spl!2spl'
                             className='h-100 w-100'
                             style={{ border: '0' }}
                             loading='lazy'
-                        </iframe>
+                        ></iframe>
                     </div>
                     <hr/>
                     <i className="fa fa-phone fa-2x" aria-hidden="true"></i>
