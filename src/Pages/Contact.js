69c52a72fab89260bb07dc36556f36a131838bf7
PaulaTomo <paulatomoiaga91@yahoo.com>
2023-03-14 15:28:39 +0100

    edit map

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -10,14 +10,15 @@ function Contact(){
                 <div className="card-contact px-sm-4 col-6">
                     <div className='col-lg-5'>
                         <iframe title="map"
-                            src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12094.57348593182!2d-74.00599512526003!3d40.72586666928451!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2598f988156a9%3A0xd54629bdf9d61d68!2sBroadway-Lafayette%20St!5e0!3m2!1spl!2spl!4v1624523797308!5m2!1spl!2spl'
-                            className='h-100 w-100'
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d89547.2776302214!2d9.095160217857808!3d45.
+                                462704104120164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.
+                                1!3m3!1m2!1s0x4786c1493f1275e7%3A0x3cffcd13c6740e8d!2sMilano%20MI!5e0!3m2!1sit!2sit!4v1678803326890!5m2!1sit!2sit"
+                                className='h-100 w-100'
                             style={{ border: '0' }}
                             loading='lazy'
                         ></iframe>
                     </div>
                     <hr/>
-                    <i className="fa fa-phone fa-2x" aria-hidden="true"></i>
                     <h5>Telefono</h5>
                     <p>+00 000 000 0000</p>
                     <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
